b3cbc8b4cdfd98593ea49e18678d39990e620414
Linderrm <112931017+Linderrm@users.noreply.github.com>
2022-10-04 09:39:38 +0200

    Add files via upload

---
+++ 9_10ora.py
@@ -0,0 +1,31 @@
+"""# Első feladat 9dk tankönyv
+IDEI_ÉV =2022
+print(type(IDEI_ÉV))
+print('Az idei év:',IDEI_ÉV,sep='-->')
+felhasználó_kora = input ("hányy éves vagy?")
+print(type(felhasználó_kora))
+print('Te moat',felhasználó_kora,'éves vagyy.')
+felhasználó_kora=int(felhasználó_kora)
+születési_év = IDEI_ÉV - felhasználó_kora
+print('Ekkor születtél',születési_év,'.'sep='')
+
+print(type(felhasználó_kora))
+felhasználó_kora = str(felhasználó_kora)
+print(type(felhasználó_kora))
+print = ('És milyen '+ felhasználó_kora +'évesnek lenni?')
+print = ('És milyen ', felhasználó_kora ,'évesnek lenni?'"""
+
+
+#Második szám 
+gondolt_szám = 4
+tipp = input("gondoltam egy számra")
+tipp == int(tipp)
+if tipp == gondolt_szám:
+    print('ügyes!')
+    print('gratulálok')
+if tipp> gondolt_szám
+    print('nagyobb szám')
+if tipp< gondolt_szám   
+    print('kisebb szám')
+    
+print('pápá')                    
