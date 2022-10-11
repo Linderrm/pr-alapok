351162f1f84a4d56b82bc38dec60803d017b7317
Linderrm <112931017+Linderrm@users.noreply.github.com>
2022-10-11 09:39:59 +0200

    Add files via upload

---
+++ 11_12ora.py
@@ -0,0 +1,28 @@
+#feladat1
+"""
+gondolt_szám = 4
+tipp = input('gondoltam egy számra. tippeld meg! ')
+tipp = int(tipp)
+if tipp == gondolt_szám:
+ print('ez az igaz ág')
+ print('ügyes')
+print('pápá.')  
+else:
+    print('hosszan gondoltál rajat')
+    print('nem érte meg')
+    print('pápá.') """
+
+    #feladat2
+szam1 = input('kérek egy számot')
+szam1 = int(szam1)
+szam2 = input('kérek egy mésik számot')
+szam2 = int(szam2)
+if szam1 == szam2:
+    print('a két szám egyenlő')
+if szam1 != szam2:
+    print('a két szám nem egyenlő')
+print('a program vége') 
+if szam1 < szam2:
+    print('szam1 kissebb mint szam2')
+else:
+    print('az szam1 nagyobb vagy egyenlő mint szam2')       
