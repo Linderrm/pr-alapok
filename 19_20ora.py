282b7af45d5678074ff2769fad234732cf7734dd
Linderrm <112931017+Linderrm@users.noreply.github.com>
2022-11-22 08:41:15 +0100

    Add files via upload

---
+++ 19_20ora.py
@@ -0,0 +1,47 @@
+"""
+print("ez \"egy\" szting \n ez már egy új sor\itt egy tabulátor van")
+"""
+"""
+print("gvcuzvewcvuzcvz",end="n\n\n")
+"""
+
+"""
+szam = 0
+while szam != 100:
+    szam = szam + 1
+    print(szam)
+print("A program vége")
+"""    
+
+
+"""
+szam = 0
+while szam != 100:
+     print(szam)
+     szam += 1
+print("A program vége")
+"""
+"""
+szam = 1
+while szam != 101:
+     print(szam)
+     szam += 1
+print("A program vége")
+"""
+"""
+szam = 1
+while True:
+     print(szam)
+     szam += 1
+print("A program vége")
+"""
+"""
+szam = 0
+while szam<100:
+     szam += 1
+     print(szam)
+print("A program vége")
+"""
+
+
+
