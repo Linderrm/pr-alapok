710dbfb47a56030e34241ce69311a1871bc1c3e4
Linderrm <112931017+Linderrm@users.noreply.github.com>
2023-02-22 07:38:45 +0100

    Add files via upload

---
+++ feladat_1.py
@@ -0,0 +1,15 @@
+#Linder Martin 10.c 2_csoport 1.feladat
+
+szam1 = int(input('Adj meg egy számot! '))
+
+
+if szam1 > 0:
+    print("A megadott szám pozitív.")
+
+elif szam1 < 0:
+    print("A megadott szám negatív.")
+
+elif szam1 == 0:
+    print("A megadott szám nulla.")    
+
+print("Proram váge! ")    
